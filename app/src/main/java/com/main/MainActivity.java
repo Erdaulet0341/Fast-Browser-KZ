b6be74bbe2bcb2d0e846d570582823b3742a8d6b
Erdaulet0341 <erdaulet0341@gmail.com>
2023-07-02 19:28:55 +0600

    updating(for cookie settings)

--- app/src/main/java/com/main/MainActivity.java
+++ app/src/main/java/com/main/MainActivity.java
@@ -11,6 +11,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.Patterns;
@@ -71,8 +72,11 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setDisplayZoomControls(false);
         webSettings.setDomStorageEnabled(true);
         webSettings.setDatabaseEnabled(true);
+
         CookieManager cookieManager = CookieManager.getInstance();
-        cookieManager.setAcceptThirdPartyCookies(webView, true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            cookieManager.setAcceptThirdPartyCookies(webView, true);
+        }
 
         AppDatabase db = Room.databaseBuilder(getApplicationContext(),
                 AppDatabase.class, "my-database").build();
